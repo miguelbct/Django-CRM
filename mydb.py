f78ecc62d556a1afd4ee16342a355a782f0658be
Miguel Mendoza <miguelctta@gmail.com>
2024-02-10 11:28:51 -0800

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='password1'
+)
+
+cursorObject=dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+print("All Done!")
